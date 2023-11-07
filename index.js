5aa3d924b3d4e6159883618151f674e9550e3047
nani-samireddy <nanisamireddy05@gmail.com>
2023-11-07 15:41:39 +0530

    updated cors

--- index.js
+++ index.js
@@ -36,21 +36,8 @@ const books = [
   { id: 2, title: "Sample Book 2", author: "Author 2", publishedYear: 2019 },
 ];
 
-app.use(cros());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://swagger-api-documentation-nodejs.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
+app.use(cros({origin: '*'}));
+
 app.use(express.json());
 
 // Get a list of books
